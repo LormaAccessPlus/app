c526896553c8736090b8ebd7cc5b5573b4ce9860
Leandro-mp4 <leandroraphael.tenorio@lorma.edu>
2025-06-08 14:33:19 +0800

    added a TopBar

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -3,10 +3,34 @@ import 'screens/drawer.dart'; // Import your drawer
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false, // Removed the debug banner
     home: Scaffold(
-      appBar: AppBar(title: Text('Main App')),
-      drawer: AppDrawer(), // Add this line
-      body: Center(child: Text('Imbotido App')),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF08695A), 
+        title: Row(
+          children: [
+        
+            const SizedBox(width: 10),
+            const Text('Lorma Access Plus'),
+          ],
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.notifications),
+            onPressed: () {
+              // Notification logic here
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: () {
+              // Profile logic here
+            },
+          ),
+        ],
+      ),
+      drawer: const AppDrawer(),
+      body: const Center(child: Text('Imbotido App')),
     ),
   ));
 }
