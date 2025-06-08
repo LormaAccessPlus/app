50b3ad37342e5f516c62355bb33e13231faaed84
Leandro-mp4 <leandroraphael.tenorio@lorma.edu>
2025-06-08 14:45:13 +0800

    Added bottom nav

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,37 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:mobile/widgets/drawer.dart';
 import 'widgets/bottom_navbar.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false, // Removed the debug banner
-    home: Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color(0xFF08695A), 
-        title: Row(
-          children: [
-        
-            const SizedBox(width: 10),
-            const Text('Lorma Access Plus'),
-          ],
-        ),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.notifications),
-            onPressed: () {
-              // Notification logic here
-            },
-          ),
-          IconButton(
-            icon: const Icon(Icons.settings),
-            onPressed: () {
-              // Profile logic here
-            },
-          ),
-        ],
-      ),
-      drawer: const AppDrawer(),
-      body: const Center(child: Text('Imbotido App')),
-    ),
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: BottomNavbar(),
   ));
 }
