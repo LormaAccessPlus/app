14fdc8b9b9c95a1ca5dd5cc47f00cadcd58e8265
johnverz22 <johnny.verzola@lorma.edu>
2025-07-07 14:24:42 +0800

    demo using old db without migration

--- web/app/Models/User.php
+++ web/app/Models/User.php
@@ -23,6 +23,9 @@ class User extends Authenticatable
         'password',
     ];
 
+    protected $table = 'users_old';
+    public $timestamps = false;
+
     /**
      * The attributes that should be hidden for serialization.
      *
